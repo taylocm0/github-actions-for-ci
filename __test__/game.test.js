717d6c3d443c73b93f180c6d2a4fa75860e0968d
Mitch Taylor <72573293+taylocm0@users.noreply.github.com>
2021-05-25 12:55:11 -0500

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,12 +1,12 @@
 const Game = require('../src/game').default
 const fs = require('fs')
-jest.setTimeout(25000)
 
 describe('App', () => {
   it('Contains the compiled JavaScript', async (done) => {
     fs.readFile('./public/main.js', 'utf8', (err, data) => {
       expect(err).toBe(null)
       expect(data).toMatchSnapshot()
+      done()
     })
   })
 })
